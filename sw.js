6da191aa0cb88f3613d4aeeaaf49ad05393194d8
alexandernoeth-bam <alexander.noeth@googlemail.com>
2026-05-26 09:26:54 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // WorkBench Service Worker v3.22.0
-const CACHE = 'workbench-v3221b';
+const CACHE = 'workbench-v3222';
 const FILES = ['./workbench.html', './manifest.json'];
 
 self.addEventListener('install', e => {
