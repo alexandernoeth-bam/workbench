1783658d4bb1daef19f8c83947fb8be3ea05f6de
alexandernoeth-bam <alexander.noeth@googlemail.com>
2026-05-26 08:49:13 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+// WorkBench Service Worker v3.22.0
+const CACHE = 'workbench-v3220';
+const FILES = ['./workbench.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // Nur eigene Requests cachen
+  if (!e.request.url.startsWith(self.location.origin)) return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fresh = fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || fresh;
+    })
+  );
+});
